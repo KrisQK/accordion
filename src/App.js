f71d9bd18a7e54180c0039accecb28e275b7e2e5
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-10-24 02:38:51 +1300

    liftingstate

--- src/App.js
+++ src/App.js
@@ -25,24 +25,28 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+    const [curOpen, setCurOpen] = useState(null);
     return (
         <div className="accordion">
             {data.map((el, i) => (
                 <AccordionItem
+                    curOpen={curOpen}
+                    onOpen={setCurOpen}
                     title={el.title}
-                    text={el.text}
                     num={i}
                     key={el.title}
-                />
+                >
+                    {el.text}
+                </AccordionItem>
             ))}
         </div>
     );
 }
 
-function AccordionItem({ num, title, text }) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+    const isOpen = num === curOpen;
     function handleToggle() {
-        setIsOpen(cur => !cur);
+        onOpen(isOpen ? null : num);
     }
     return (
         <div
@@ -52,7 +56,7 @@ function AccordionItem({ num, title, text }) {
             <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
             <p className="text">{title}</p>
             <p className="icon">{isOpen ? "-" : "+"}</p>
-            {isOpen && <div className="content-box">{text}</div>}
+            {isOpen && <div className="content-box">{children}</div>}
         </div>
     );
 }
